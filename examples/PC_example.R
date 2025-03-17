5834295b7907b7abc44ca2ee4e43fe8a1b2d0b2d
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-03-17 11:45:55 +0100

    Make example more simple

--- examples/PC_example.R
+++ examples/PC_example.R
@@ -1,28 +1,23 @@
 # generate data
 devtools::load_all()
 set.seed(123)
-n <- 100000
-p <- 5
+n <- 10**4
+p <- 3
 dag <- matrix(0, p, p)
-dag[1, 2] <- 1
-dag[1, 3] <- 1
-dag[2, 4] <- 1
-dag[3, 4] <- 1
-dag[4, 5] <- 1
-cov <- matrix(0.5, p, p)
-diag(cov) <- 1
-data <- MASS::mvrnorm(n, rep(0, p), cov)
-df <- data.frame(data)
-colnames(df) <- paste0("V", 1:p)
+V1 <- rnorm(n, 0, 1)
+V2 <- 0.5 * V1 + rnorm(n, 0, 0.5)
+V3 <- V2 + rnorm(n, 0, 0.1)
+V4 <- V3 + rnorm(n, 0, 1)
+V5 <- rnorm(n, 0, 1)
+V6 <- rnorm(n, 0, 1) + 0.7 * V5
+# into dataframe
+df <- data.frame(V1, V2, V3, V4, V5, V6)
 
-# pc example
-ts_obj <- TetradSearch$new(df)
-ts_obj$use_fisher_z()
-ts_obj$run_pc() #
-ts_obj$get_dot() |> cat()
+# disco call example
 
 # fges example
-ts_obj2 <- TetradSearch$new(df)
-ts_obj2$use_sem_bic(penalty_discount = 2, structure_prior = 0, sem_bic_rule = 1)
-ts_obj2$run_fges()
-ts_obj2$get_dot() |> cat()
+ts <- TetradSearch$new()
+ts$set_data(df)
+ts$set_score("sem_bic", penalty_discount = 2, structure_prior = 0, sem_bic_rule = 1)
+ts$run_fges()
+ts$get_dot() |> cat()
