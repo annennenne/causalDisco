bfd4a2242740093d2c25da77e9bf1e6d3fdf13bd
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-04-08 15:38:47 +0200

    Remove @param comment

--- R/disco.R
+++ R/disco.R
@@ -2,7 +2,6 @@
 #' @param data a data frame
 #' @param method an algorithm/method object
 #' @param knowledge a knowledge object
-#' @param engine a character describing the underlying engine to use=
 #' @export
 disco <- function(data, method, knowledge = NULL) {
   if (!is.null(knowledge)) {
