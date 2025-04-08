9d041aaad796bda1df2c4d3b659c3f87565a1926
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-04-08 17:00:20 +0200

    Add ges method (only for tetrad)

---
+++ R/ges_method.R
@@ -0,0 +1,42 @@
+#' @export
+ges <- function(engine = "tetrad", score, ...) {
+  engine <- tolower(engine)
+
+  switch(engine,
+    tetrad = ges_tetrad(score, ...),
+    pcalg = ges_pcalg(score, ...),
+    bnlearn = ges_bnlearn(score, ...),
+    stop("Unsupported engine: ", engine)
+  )
+}
+
+
+# Set available engines
+attr(pc, "engines") <- c("tetrad")
+
+ges_tetrad <- function(score, ...) {
+  # to do: fix arguments passing correctly
+  search <- TetradSearch$new()
+  # to do: set_params(args_to_pass)
+  search$set_score(score, ...)
+  search$set_alg("fges")
+  runner <- list(
+    set_knowledge = function(knowledge) {
+      search$set_knowledge(knowledge)
+    },
+    run = function(data) {
+      search$run_search(data)
+      search$get_dot() # todo: change to igraph later
+    }
+  )
+  return(runner)
+}
+
+ges_pcalg <- function(score, ...) {
+  stop("Not implemented yet.")
+}
+
+
+ges_bnlearn <- function(score, ...) {
+  stop("Not implemented yet.")
+}
