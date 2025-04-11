569aff45b6663877a664b0efd5795b48d61f2a33
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-04-11 10:08:12 +0200

    fci method function alpha version. Works for tetrad, but have not implemented it for pcalg yet

---
+++ R/fci_method.R
@@ -0,0 +1,100 @@
+#' @export
+fci <- function(engine = "tetrad", test, alpha = 0.05, ...) {
+  engine <- tolower(engine)
+
+  switch(engine,
+    tetrad = fci_tetrad(test, alpha, ...),
+    pcalg = fci_pcalg(test, alpha, ...),
+    bnlearn = fci_bnlearn(test, alpha, ...),
+    stop("Unsupported engine: ", engine)
+  )
+}
+# Set available engines
+attr(fci, "engines") <- c("tetrad", "pcalg")
+
+fci_tetrad <- function(test, alpha, ...) {
+  search <- TetradSearch$new()
+  args <- list(...)
+  args_to_pass <- check_args_and_return_passable_args(args, "tetrad", test, search)
+  if (length(args_to_pass$test_args) != 0) {
+    search$set_test(test, alpha, args_to_pass$test_args)
+  } else {
+    search$set_test(test, alpha)
+  }
+  if (length(args_to_pass$alg_args) != 0) {
+    search$set_alg("fci", args_to_pass$alg_args)
+  } else {
+    search$set_alg("fci")
+  }
+  runner <- list(
+    set_knowledge = function(knowledge) {
+      search$set_knowledge(knowledge)
+    },
+    run = function(data) {
+      search$run_search(data)
+      search$get_dot() # todo: change to igraph later
+    }
+  )
+  return(runner)
+}
+
+
+check_args_and_return_passable_args <- function(args, engine, test = NULL, search = NULL) {
+  # Check if the engine is supported
+  if (!(engine %in% engine_registry)) {
+    stop(
+      "Engine ", paste(engine), "is not supported. Supported engines are: ",
+      paste(engine_registry, collapse = ", ")
+    )
+  }
+  if (engine %in% c("pcalg", "bnlearn")) {
+    if (engine == "pcalg") {
+      engine_args <- names(formals(pcalg::fci))
+    } else if (engine == "bnlearn") {
+      stop("bnlearn engine is not supported yet.")
+      engine_args <- names(formals(bnlearn::fci))
+    }
+
+    args_to_pass_to_engine <- args[names(args) %in% engine_args]
+
+    # Check if any arguments are not in pcalg::fci or bnlearn::fci
+    args_not_in_engine_args <- setdiff(names(args), engine_args)
+    if (length(args_not_in_engine_args) > 0) {
+      warning(
+        paste0("The following arguments are not used in ", engine, "::fci : "),
+        paste(args_not_in_engine_args, collapse = ", ")
+      )
+    }
+    return(args_to_pass_to_engine)
+  } else if (engine == "tetrad") {
+    if (is.null(search)) {
+      stop("Search object must be provided for checks.")
+    }
+    if (is.null(test)) {
+      stop("Test must be provided for Tetrad engine checks.")
+    }
+    engine_args_alg <- search$get_parameters_for_function("fci")
+    engine_args_test <- search$get_parameters_for_function(test)
+
+    args_to_pass_to_engine_alg <- args[names(args) %in% engine_args_alg]
+    args_to_pass_to_engine_test <- args[names(args) %in% engine_args_test]
+
+    # Check if any arguments are not in tetrad's pc or test
+    args_not_in_engine_args <- setdiff(
+      names(args),
+      c(engine_args_alg, engine_args_test)
+    )
+    if (length(args_not_in_engine_args) > 0) {
+      warning(
+        "The following arguments are not used in Tetrad's pc or test: ",
+        paste(args_not_in_engine_args, collapse = ", ")
+      )
+    }
+    return(list(
+      alg_args = args_to_pass_to_engine_alg,
+      test_args = args_to_pass_to_engine_test
+    ))
+  } else {
+    stop("Unsupported engine: ", engine)
+  }
+}
