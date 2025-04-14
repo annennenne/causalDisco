deccd4d0288df5b0e531bb318b52ed8cfe723782
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-04-14 13:31:40 +0200

    Remove check_args function from pc method

--- R/pc_method.R
+++ R/pc_method.R
@@ -15,7 +15,7 @@ attr(pc, "engines") <- c("tetrad", "pcalg")
 pc_tetrad <- function(test, alpha, ...) {
   search <- TetradSearch$new()
   args <- list(...)
-  args_to_pass <- check_args_and_return_passable_args(args, "tetrad", test, search)
+  args_to_pass <- check_args_and_distribute_args(search, args, "tetrad", "pc", test)
   if (length(args_to_pass$test_args) != 0) {
     search$set_test(test, alpha, args_to_pass$test_args)
   } else {
@@ -53,65 +53,4 @@ pc_pcalg <- function(test, alpha, ...) {
       search$run_search(data)
     }
   )
-}
-
-
-check_args_and_return_passable_args <- function(args, engine, test = NULL, search = NULL) {
-  # Check if the engine is supported
-  if (!(engine %in% engine_registry)) {
-    stop(
-      "Engine ", paste(engine), "is not supported. Supported engines are: ",
-      paste(engine_registry, collapse = ", ")
-    )
-  }
-  if (engine %in% c("pcalg", "bnlearn")) {
-    if (engine == "pcalg") {
-      engine_args <- names(formals(pcalg::pc))
-    } else if (engine == "bnlearn") {
-      stop("bnlearn engine is not supported yet.")
-      engine_args <- names(formals(bnlearn::pc))
-    }
-
-    args_to_pass_to_engine <- args[names(args) %in% engine_args]
-
-    # Check if any arguments are not in pcalg::pc
-    args_not_in_engine_args <- setdiff(names(args), engine_args)
-    if (length(args_not_in_engine_args) > 0) {
-      warning(
-        paste0("The following arguments are not used in ", engine, "::pc : "),
-        paste(args_not_in_engine_args, collapse = ", ")
-      )
-    }
-    return(args_to_pass_to_engine)
-  } else if (engine == "tetrad") {
-    if (is.null(search)) {
-      stop("Search object must be provided for checks.")
-    }
-    if (is.null(test)) {
-      stop("Test must be provided for Tetrad engine checks.")
-    }
-    engine_args_alg <- search$get_parameters_for_function("pc")
-    engine_args_test <- search$get_parameters_for_function(test)
-
-    args_to_pass_to_engine_alg <- args[names(args) %in% engine_args_alg]
-    args_to_pass_to_engine_test <- args[names(args) %in% engine_args_test]
-
-    # Check if any arguments are not in tetrad's pc or test
-    args_not_in_engine_args <- setdiff(
-      names(args),
-      c(engine_args_alg, engine_args_test)
-    )
-    if (length(args_not_in_engine_args) > 0) {
-      warning(
-        "The following arguments are not used in Tetrad's pc or test: ",
-        paste(args_not_in_engine_args, collapse = ", ")
-      )
-    }
-    return(list(
-      alg_args = args_to_pass_to_engine_alg,
-      test_args = args_to_pass_to_engine_test
-    ))
-  } else {
-    stop("Unsupported engine: ", engine)
-  }
 }
