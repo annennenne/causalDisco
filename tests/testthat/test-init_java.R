f3f7f48e7ca8742d8ef5a36743947f0aac09dcec
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-08-04 16:54:31 +0200

    Tests: testing new onAttach functions

---
+++ tests/testthat/test-init_java.R
@@ -0,0 +1,38 @@
+library(testthat)
+library(withr)
+
+test_that("init_java starts JVM once and adds jars", {
+  with_mock_rjava({
+    pkg <- "causalDisco"
+    ns <- asNamespace(pkg)
+
+    # ---- first stub: return only tmp --------------------------------
+    tmp <- tempfile(fileext = ".jar")
+    dir.create(dirname(tmp), recursive = TRUE, showWarnings = FALSE)
+    file.create(tmp)
+
+    # save & stub
+    orig <- get("find_tetrad_jars", envir = ns)
+    assignInNamespace("find_tetrad_jars", function() tmp, pkg)
+
+    # exercise
+    causalDisco:::init_java(heap = "4g")
+    expect_true(.j_state$started)
+    expect_true(tmp %in% .j_state$class_path)
+
+    # restore
+    assignInNamespace("find_tetrad_jars", orig, pkg)
+
+    # ---- second stub: return tmp and tmp2 ---------------------------
+    tmp2 <- tempfile(fileext = ".jar")
+    file.create(tmp2)
+    assignInNamespace("find_tetrad_jars", function() c(tmp, tmp2), pkg)
+
+    # exercise again
+    causalDisco:::init_java(heap = "4g")
+    expect_true(all(c(tmp, tmp2) %in% .j_state$class_path))
+
+    # final restore (cleanup)
+    assignInNamespace("find_tetrad_jars", orig, pkg)
+  })
+})
