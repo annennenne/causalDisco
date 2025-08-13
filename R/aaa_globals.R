9aff8ef2ac70f00219cb9b3640a74ecc98c39406
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-08-13 15:55:45 +0200

    Make globals prettier

--- R/aaa_globals.R
+++ R/aaa_globals.R
@@ -1,1 +1,6 @@
-utils::globalVariables(c("from", "to", "tier", "pair", "tier_from", "tier_to", "desc", "status", "var", "label", "node1", "node2", "edge_type", "rank_from", "rank_to"))
+utils::globalVariables(
+  c(
+    "from", "to", "tier", "pair", "tier_from", "tier_to", "desc", "status",
+    "var", "label", "node1", "node2", "edge_type", "rank_from", "rank_to"
+  )
+)
