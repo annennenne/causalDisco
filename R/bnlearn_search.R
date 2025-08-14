db38f53febe5f0c1a0dcd1712e15ae6498e2b74d
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-08-14 15:33:02 +0200

    Fix RMC CHECK notes

--- R/bnlearn_search.R
+++ R/bnlearn_search.R
@@ -295,6 +295,8 @@ bnlearnSearch <- R6Class(
     #' @param method Character naming the algorithm to use.
     #' @param args A list of additional arguments to pass to the algorithm.
     #' @importFrom purrr partial
+    #' @importFrom bnlearn pc.stable gs iamb fast.iamb inter.iamb iamb.fdr
+    #' mmpc si.hiton.pc hpc hc tabu mmhc rsmax2 h2pc chow.liu aracne
     set_alg = function(method, args = NULL) {
       method <- tolower(method)
 
