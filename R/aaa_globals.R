599f502225b6db88dc4d54fad944808305848dab
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-11-06 16:52:06 +0100

    New global variables used for converting EssGraphs

--- R/aaa_globals.R
+++ R/aaa_globals.R
@@ -2,6 +2,6 @@ utils::globalVariables(
   c(
     ".src", "from", "to", "tier", "pair", "tier_from", "tier_to", "desc",
     "status", "var", "label", "node1", "node2", "edge_type", "rank_from",
-    "rank_to"
+    "rank_to", "has_fw", "has_bw", "canon_from", "canon_to"
   )
 )
