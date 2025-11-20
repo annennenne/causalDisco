6540bab6c101a9be27a8da26b1662c587e3639ae
BjarkeHautop <bjarke.kristensen@sund.ku.dk>
2025-11-20 14:11:06 +0100

    Fix bug in probmat2amat where bpco always used cpdag (probably legacy code?)

--- R/probmat2amat.R
+++ R/probmat2amat.R
@@ -20,7 +20,7 @@
 #' @param graph_criterion Which criterion to check if the output graph fulfills for the bpco
 #' method. Should be one of \code{"dag"}, \code{"pdag"} or \code{"cpdag"}  or
 #' \code{NULL}. Choosing \code{NULL} (the default) puts no further restrictions on the output.
-#' See \code{\link[pcalg]{isValidGraph}} for definitions.
+#' See \code{\link[pcalg]{pcalg::isValidGraph}} for definitions.
 #' @param deletesym If \code{TRUE}, edges are deleted symmetrically in the bcpo method. This means that instead
 #' of removing arrowheads (setting singular elements to 0), the procedure removes full edges (setting both potential
 #' arrowheads for the given edge to zero). This only makes a difference if the graph may include undirected edges,
@@ -43,7 +43,7 @@
 #'
 #' # Convert to adjacency matrix using BPCO-method (threshold = 0.5)
 #' probmat2amat(pmat, threshold = 0.5, method = "bpco")
-#'
+#' 
 #' @export
 probmat2amat <- function(probmat, threshold, method = "cutoff",
                          keep_vnames = TRUE, graph_criterion = "pdag",
@@ -73,7 +73,7 @@ probmat2amat <- function(probmat, threshold, method = "cutoff",
 
     new <- probmat2amat(probmat, threshold, "cutoff")
     ord <- rev(ord)
-    new_is_valid <- is_cpdag(new)
+    new_is_valid <- pcalg::isValidGraph(new, type = graph_criterion)
 
     badmat <- t(matrix(1:(p^2), p, p))
 
@@ -82,10 +82,10 @@ probmat2amat <- function(probmat, threshold, method = "cutoff",
       new[deleteind] <- 0
       if (deletesym) new[which(badmat == deleteind)] <- 0
 
-      new_is_valid <- is_cpdag(new)
+      new_is_valid <- pcalg::isValidGraph(new, type = graph_criterion)
       if (!new_is_valid) {
         trynew <- correspondingCpdag(new)
-        if (is_cpdag(trynew)) {
+        if (pcalg::isValidGraph(trynew, type = graph_criterion)) {
           new <- trynew
           new_is_valid <- TRUE
         }
