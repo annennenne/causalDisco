d263cf3204ae6aa9f89297dfdbc861bde0602c4a
Bjarke Hautop <bjarke.hautop@gmail.com>
2026-01-02 10:16:58 +0100

    Fix broken test

--- R/zzz.R
+++ R/zzz.R
@@ -30,9 +30,11 @@
     return()
   }
 
-  if (is_interactive() &&
-    is.null(getOption("java.heap.size")) &&
-    !nzchar(Sys.getenv("JAVA_HEAP_SIZE", unset = ""))) {
+  if (
+    is_interactive() &&
+      is.null(getOption("java.heap.size")) &&
+      !nzchar(Sys.getenv("JAVA_HEAP_SIZE", unset = ""))
+  ) {
     options(java.heap.size = ask_heap_size())
   }
 
@@ -42,7 +44,11 @@
 
   # Check Tetrad installation
   tetrad_status <- check_tetrad_install()
-  tetrad_version <- ifelse(tetrad_status$installed, tetrad_status$version, "not installed")
+  tetrad_version <- ifelse(
+    tetrad_status$installed,
+    tetrad_status$version,
+    "not installed"
+  )
 
   current_heap_size <- NA
   java_initialized <- FALSE
@@ -61,28 +67,42 @@
   # Build startup message
   msg <- paste0(
     "causalDisco startup:\n",
-    "  Java heap size requested: ", round(heap_gb, 2), " GB\n",
-    "  Tetrad version: ", tetrad_version, "\n"
+    "  Java heap size requested: ",
+    round(heap_gb, 2),
+    " GB\n",
+    "  Tetrad version: ",
+    tetrad_version,
+    "\n"
   )
 
   if (java_initialized) {
     if (abs(current_heap_size - heap_gb) > 0.1) {
       msg <- paste0(
         msg,
-        "  WARNING: Java heap is ", current_heap_size,
-        " GB but you requested ", heap_gb,
+        "  WARNING: Java heap is ",
+        current_heap_size,
+        " GB but you requested ",
+        heap_gb,
         " GB. Restart R to change the heap.\n"
       )
     } else {
       msg <- paste0(
         msg,
-        "  Java successfully initialized with ", current_heap_size, " GB.\n"
+        "  Java successfully initialized with ",
+        current_heap_size,
+        " GB.\n"
       )
     }
   } else if (tetrad_status$installed) {
-    msg <- paste0(msg, "  WARNING: Java initialization failed. Check your Java setup.\n")
+    msg <- paste0(
+      msg,
+      "  WARNING: Java initialization failed. Check your Java setup.\n"
+    )
   } else {
-    msg <- paste0(msg, "  Tetrad is not installed. Run install_tetrad() to install it.\n")
+    msg <- paste0(
+      msg,
+      "  Tetrad is not installed. Run install_tetrad() to install it.\n"
+    )
   }
 
   msg <- paste0(
