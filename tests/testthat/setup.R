fb9c4276df8636093b34703cf6d5b735d2f14bf6
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-01-28 11:35:45 +0100

    Make comments about what to do before CRAN submission in setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,8 @@
 # Ensures Tetrad is installed for when running it on GitHub Actions
+# Move this to GitHub Actions workflow? Sometimes Tetrad runs out of memory (rare though) which
+# would break tests ran on CRAN.
+# Could keep this with skip_on_cran() so it auto downloads when a user runs tests locally?
+# (Note GA runs as if on CRAN regarding skip_on_cran())
 tetrad_installed <- check_tetrad_install()$installed
 if (!tetrad_installed) {
   install_tetrad()
