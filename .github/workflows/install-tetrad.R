36f426700899a15c3a83d2a5ef899853949f867c
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-02-04 13:30:39 +0100

    Add comments specifying why this approach is done and delete old R file

--- .github/workflows/install-tetrad.R
+++ .github/workflows/install-tetrad.R
@@ -1,3 +1,6 @@
+# Ensure Tetrad is installed for workflows.
+# - Not done in setup.R, since that would install it on CRAN too (workflows runs as CRAN)
+# to avoid rare memory issues that could cause false negatives.
 source("R/zzz.R")
 source("R/install-tetrad.R")
 install_tetrad(version = .default_tetrad_version)
--- .github/workflows/pkgdown-install-tetrad.R
+++
@@ -1,3 +0,0 @@
-source("R/zzz.R")
-source("R/install-tetrad.R")
-install_tetrad(version = .default_tetrad_version)
