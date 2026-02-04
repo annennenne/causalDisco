4917d780656c399603ae8e1c0a2ebe0c6902baa4
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-02-04 13:07:11 +0100

    Move install tetrad away from CRAN and instead inside the workflows

--- .github/workflows/install-tetrad.R
+++ .github/workflows/install-tetrad.R
@@ -1,3 +1,4 @@
 source("R/zzz.R")
 source("R/install-tetrad.R")
 install_tetrad(version = .default_tetrad_version)
+check_tetrad_install(version = .default_tetrad_version)
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,6 +3,7 @@
 # would break tests ran on CRAN.
 # Could keep this with skip_on_cran() so it auto downloads when a user runs tests locally?
 # (Note GA runs as if on CRAN regarding skip_on_cran())
+skip_on_cran()
 tetrad_installed <- check_tetrad_install()$installed
 if (!tetrad_installed) {
   install_tetrad()
