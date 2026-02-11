bc6395614f41adc316fd59086c2c9bc92e3ba066
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-02-11 09:56:47 +0100

    Update DESCRIPTION

--- R/causalDisco-package.R
+++ R/causalDisco-package.R
@@ -1,8 +1,6 @@
 #' @title causalDisco: Causal Discovery in R
 #' @name causalDisco-package
 #' @section System requirements:
-#' Cargo (Rust's package manager), rustc >= 1.80.0, xz.
-#'
 #' Java (>= 21) for Tetrad functionality (optional; includes installer; see inst/java/LICENSE and inst/java/COPYRIGHT);
 #' Source code: \url{https://github.com/cmu-phil/tetrad}.
 "_PACKAGE"
