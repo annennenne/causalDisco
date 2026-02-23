580066de07d7cfd48e655a1e4cfcefda1db157b3
Bjarke Hautop <bjarke.hautop@gmail.com>
2026-02-23 13:11:14 +0100

    Fix GA install Tetrad workflow

--- .github/workflows/install-tetrad.R
+++ .github/workflows/install-tetrad.R
@@ -1,11 +1,9 @@
-source("R/zzz.R")
-source("R/install-tetrad.R")
+library(causalDisco)
 
 # Install Tetrad to package cache
 install_tetrad(
-  version = .default_tetrad_version,
   force = FALSE,
   quiet = FALSE
 )
 
-verify_tetrad(version = .default_tetrad_version)
+verify_tetrad(version)
